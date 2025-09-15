5a2fae061220aa06dfddacc05f801828ad81dd5a
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-15 07:54:43 +0100

    add index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JGDM: Dom Project - Personal Dashboard</title>
+</head>
+<body>
+    
+    <h1>JGDM: Dom Project - Personal Dashboard</h1>
+
+    <p>index.php</p>
+
+    <a href="https://github.com/jg-digital-media/jgdm_personal_dashboard_dom" target="_blank">Github Repo</a>
+</body>
+</html>
